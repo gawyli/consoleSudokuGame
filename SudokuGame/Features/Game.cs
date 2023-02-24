dcd7d2cdb0c9b7289167652cf07a75a916607b9a
gawyli <pawel.wisniewski@stormid.com>
2023-02-24 23:34:20 +0000

    Task61:Created menu to the game with submenu

--- SudokuGame/Features/Game.cs
+++ SudokuGame/Features/Game.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SudokuGame.Models
+namespace SudokuGame.Features
 {
-    public class Player
+    public class Game
     {
-        public Guid Id { get; set; }
-        public string Nickname { get; set; }
     }
 }
---
+++ SudokuGame/Features/Menu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SudokuGame.Features
+{
+    public class Menu
+    {
+        const string TITLE = @"
+             __              _         _                 ___                         
+            / _\  _   _   __| |  ___  | | __ _   _      / _ \  __ _  _ __ ___    ___ 
+            \ \  | | | | / _` | / _ \ | |/ /| | | |    / /_\/ / _` || '_ ` _ \  / _ \
+            _\ \ | |_| || (_| || (_) ||   < | |_| |   / /_\\ | (_| || | | | | ||  __/
+            \__/  \__,_| \__,_| \___/ |_|\_\ \__,_|   \____/  \__,_||_| |_| |_| \___|
+                                                        ";
+        private int SelectedIndex { get; set; } = 0;
+
+        public Menu()
+        {
+        }
+
+        private void DisplayOptions(string prompt, string[] options)
+        {
+            WriteLine(TITLE);
+            WriteLine(prompt);
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                string currentOption = options[i];
+                string prefix;
+
+                if (i == SelectedIndex)
+                {
+                    prefix = "->";
+                    ForegroundColor = ConsoleColor.Magenta;
+                    BackgroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    prefix = " ";
+                    ForegroundColor = ConsoleColor.Yellow;
+                    BackgroundColor = ConsoleColor.Magenta;
+                }
+
+                WriteLine($"{prefix} << {currentOption} >>");
+
+            }
+            ResetColor();
+        }
+
+        public int Run(string prompt, string[] options)
+        {
+            SelectedIndex = 0;
+            ConsoleKey keyPressed;
+
+            do
+            {
+                Clear();
+                DisplayOptions(prompt, options);
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
+
+

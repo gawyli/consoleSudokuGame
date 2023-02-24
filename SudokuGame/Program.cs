95a0042a3737762b9c98c8e4f1be900e73ee106d
gawyli <pawel.wisniewski@stormid.com>
2023-02-24 19:51:04 +0000

    76:Created Player class and function to get player nickname

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SudokuGame.Models;
+
+
+Player user = new Player();
+
+
+Console.WriteLine("helloWord");
+Console.ReadKey(true);

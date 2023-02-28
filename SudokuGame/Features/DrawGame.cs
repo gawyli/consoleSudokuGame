17357e6381028ee14a1b2c04762f060c39cc569e
gawyli <pawel.wisniewski@stormid.com>
2023-02-28 11:16:27 +0000

    62: Start new game & generate new board

---
+++ SudokuGame/Features/DrawGame.cs
@@ -0,0 +1,66 @@
+﻿using SudokuGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuGame.Features
+{
+    public class DrawGame
+    {
+        public static void PopulateSudoku(int[,] board)   // Draw the board
+        {
+            //var elapsedMs = watch.ElapsedMilliseconds;
+            //Console.WriteLine("Time execution = " + elapsedMs.ToString() + "ms" + "\n");
+
+
+            int isColThree = 0, isRowThree = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                isRowThree++;
+
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(" " + board[i, j].ToString() + " ");
+                    isColThree++;
+
+                    if (isColThree == 3)
+                    {
+                        Console.Write(" | ");
+                        isColThree = 0;
+                    }
+                }
+                Console.WriteLine();
+
+                if (isRowThree == 3)
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        Console.Write(" - ");
+                    }
+                    Console.WriteLine();
+
+                    isRowThree = 0;
+                }
+            }
+
+            int total = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        total++;
+                    }
+                }
+            }
+            int emptyCells = 81 - total;
+
+            Console.WriteLine("\n" + "Total: " + emptyCells.ToString());
+        }
+    }
+}
---
+++ SudokuGame/Navigation/Menu.cs
@@ -0,0 +1,46 @@
+﻿using SudokuGame.Features;
+using SudokuGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuGame.Navigation
+{
+    public class Menu : MenuLogic
+    {
+        static readonly UserConfig _userConfig = UserConfig.GetUserConfig();
+        public static void GameMenu()
+        {
+            string menuPrompt = "Select:";
+            string timePrompt = "Select time constraints:";
+            string levelPrompt = "Select level:";
+            string[] menuOptions = { "Start game", "Load game", "Ranking", "Exit game" };
+            string[] timeOptions = { "None", "10min", "15min", "20min", "25min" };
+            string[] levelOptions = { "Easy", "Medium", "Hard" };
+
+            int selectedIndex = RunMenu(menuPrompt, menuOptions);
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    _userConfig.Time = RunMenu(timePrompt, timeOptions);
+                    _userConfig.Level = RunMenu(levelPrompt, levelOptions);
+                    Console.WriteLine("Enter your username: ");
+                    _userConfig.Nickname = Console.ReadLine()!;
+                    GameManager.StartGame();
+                    break;
+                case 1:
+                    GameManager.LoadGame();
+                    break;
+                case 2:
+                    GameManager.Ranking();
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+            }
+        }   
+    }
+}

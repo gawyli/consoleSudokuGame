24fb51864137f9018c14a79b7515ede6a0f20428
gawyli <pawel.wisniewski@stormid.com>
2023-02-28 11:16:44 +0000

    Clean up program.cs

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -5,19 +5,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.AccessControl;
 
-
-
-
 while (true)
 {
     Menu.GameMenu();
-}
-
-
-
-
-
-void StartTheGame(int timeIndex, int levelIndex)
-{
-    //GameManager.LoadGame(levelIndex);
 }
